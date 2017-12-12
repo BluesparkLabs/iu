7937ff56fa30b9dfda6cbabde4c3caf801727fd7
James Wilson <jrwilson3@gmail.com>
2017-12-11 22:15:58 -0500

    Issue #2930082 by jwilson3: Expose color palette data in a helper
    function and add a 'none' option.

--- theme-settings.php
+++ theme-settings.php
@@ -9,80 +9,10 @@ function iu_form_system_theme_settings_alter(&$form, \Drupal\Core\Form\FormState
     return;
   }
 
-  $options = [
-    'crimson' => [
-      'name'=>'Crimson',
-      'hex' => '#990000',
-      'class' => 'bg-crimson bg-dark',
-    ],
-    'mahogany' => [
-      'name'=>'Mahogany',
-      'hex' => '#4a3c31',
-      'class' => 'bg-mahogany bg-dark',
-    ],
-    'gold-dark' => [
-      'name'=>'Dark Gold',
-      'hex' => '#dc8823',
-      'class' => 'bg-gold-dark',
-    ],
-    'gold' => [
-      'name'=>'Gold',
-      'hex' => '#f1be48',
-      'class' => 'bg-gold',
-    ],
-    'mint-dark' => [
-      'name'=>'Dark Mint',
-      'hex' => '#285c4d',
-      'class' => 'bg-mint-dark bg-dark',
-    ],
-    'mint' => [
-      'name'=>'Mint',
-      'hex' => '#008264',
-      'class' => 'bg-mint bg-dark',
-    ],
-    'midnight-dark' => [
-      'name'=>'Dark Midnight',
-      'hex' => '#01426a',
-      'class' => 'bg-midnight-dark bg-dark',
-    ],
-    'midnight' => [
-      'name'=>'Midnight',
-      'hex' => '#006298',
-      'class' => 'bg-midnight bg-dark',
-    ],
-    'majestic-dark' => [
-      'name'=>'Dark Majestic',
-      'hex' => '#512a44',
-      'class' => 'bg-majestic-dark bg-dark',
-    ],
-    'majestic' => [
-      'name'=>'Majestic',
-      'hex' => '#66435a',
-      'class' => 'bg-majestic bg-dark',
-    ],
-    'limestone-dark' => [
-      'name'=>'Dark Limestone',
-      'hex' => '#83786f',
-      'class' => 'bg-limestone-dark bg-dark',
-    ],
-    'limestone' => [
-      'name'=>'Limestone',
-      'hex' => '#aca39a',
-      'class' => 'bg-limestone',
-    ],
-    'black' => [
-      'name'=>'Black',
-      'hex' => '#191919',
-      'class' => 'bg-black bg-dark',
-    ],
-    'gray' => [
-      'name'=>'Light Gray',
-      'hex' => '#edebeb',
-      'class' => 'bg-gray',
-    ],
-  ];
+  $colors = _iu_secondary_color_palette_options();
+  $options = [];
 
-  foreach ($options as $key => $color) {
+  foreach ($colors as $key => $color) {
     $options[$key] = '
 <div class="color-block '. $color['class'] . '">
   <div class="color-name">' . $color['name'] . '</div>
