829f3d3574a471d335132ef7c396a7e5f3ecd371
James Wilson <jrwilson3@gmail.com>
2018-08-16 15:33:53 -0500

    Issue #2980863 by jwilson3: Theme skip links

--- js/init.js
+++ js/init.js
@@ -20,5 +20,51 @@
         // delete IU.uiModules['accordion'];
 
         IU.init && IU.init();
+
+        // Account for absolutely positioned elements when animating a scroll effect.
+        IU.uiDisplace = function() {
+
+            // Account for IU Framework's sticky nav or branding bar, and bigNav setting.
+            var displace = (IU.settings.bigNav) ? 68 : 55;
+
+            // Account for Drupal Toolbar.
+            if ($('body').hasClass('toolbar-fixed')) {
+                displace += 39;
+            }
+            // Account for Drupal Toolbar tray.
+            if ($('body').hasClass('toolbar-tray-open')
+                && $('body').hasClass('toolbar-horizontal')) {
+                displace += 40;
+            }
+            return displace;
+        };
+
+        // Override default skipNav helper to integrate with Drupal Toolbar.
+        IU.addHelper('skipNav', function() {
+            var scope = this;
+
+            $("#skipnav a").on('click', function(event) {
+                var target = $(this.hash);
+
+                if (target.length) {
+                    $('html, body').animate({
+                        scrollTop: target.offset().top - IU.uiDisplace(),
+                    }, 500);
+
+                    target.attr('tabindex', 0).on('blur focusout', function () {
+                        $(this).removeAttr('tabindex');
+                    }).focus();
+
+                    if (target.attr('id') === 'search') {
+                        $("#toggles a.search-toggle")[0].click();
+                        return;
+                    }
+                    else if (target.attr('id') === 'nav-main' && IU.$window.width() < 1025 ) {
+                        $('#toggles [data-toggle]').click();
+                        return;
+                    }
+                }
+            });
+        });
     });
 })(jQuery, window, window.document);
